1e599057edf76ca14c201a369dd1a0ff2ccfedd8
Crowdbotics <team@crowdbotics.com>
2024-04-20 19:28:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tobacco-go-47651",
+  url: "https://tobacco-go-47651.botics.co",
+  api: "https://tobacco-go-47651.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
